9926aa9cdfeb10cd1e57708366cdcc62b0370723
Patrick Perini <pcperini@aim.com>
2012-04-30 12:22:56 -0400

    Initial Commit

---
+++ PCHTTP/PCHTTPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCHTTPAppDelegate.h
+//  PCHTTP
+//
+//  Created by Patrick Perini on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PCHTTPAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PCHTTP/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PCHTTP
+//
+//  Created by Patrick Perini on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
