ab02c19205e285d2e583cff428221b233fad2369
Patrick Perini <pcperini@gmail.com>
2012-07-27 15:10:33 -0400

    add all official HTTP status codes to PCHTTResponse
    some of the old ones were refactored to favor, where possible, an Objective-C style verbosity.

--- PCHTTP/PCHTTPResponse.h
+++ PCHTTP/PCHTTPResponse.h
@@ -12,14 +12,56 @@
  */
 typedef enum
 {
-    PCHTTPSuccessfulResponse        = 200,
-    PCHTTPBadRequestResponse        = 400,
-    PCHTTPUnauthorizedResponse      = 401,
-    PCHTTPForbiddenResponse         = 403,
-    PCHTTPNotFoundResponse          = 404,
-    PCHTTPMethodNotAllowedResponse  = 405,
-    PCHTTPConflictResponse          = 409,
-    PCHTTPServerError               = 500
+    // Informational
+    PCHTTPShouldContinue                  = 100,
+    PCHTTPWillSwitchProtocols             = 101,
+    
+    // Successful
+    PCHTTPSuccess                         = 200,
+    PCHTTPResourceWasCreated              = 201,
+    PCHTTPRequestWasAccepted              = 202,
+    PCHTTPNonAuthoritativeSuccess         = 203,
+    PCHTTPNoContent                       = 204,
+    PCHTTPContentWasReset                 = 205,
+    PCHTTPPartialContent                  = 206,
+    
+    // Redirection
+    PCHTTPMutltipleChoices                = 300,
+    PCHTTPResourcePermanentlyMoved        = 301,
+    PCHTTPResourceWasFound                = 302,
+    PCHTTPSeeOtherResource                = 303,
+    PCHTTPResourceHasNotBeenModified      = 304,
+    PCHTTPRequestMustBeProxied            = 305,
+    PCHTTPResourceHasBeenTemporarilyMoved = 307,
+    
+    // Client Errors
+    PCHTTPBadRequest                       = 400,
+    PCHTTPUnauthorizedRequest              = 401,
+    PCHTTPPaymentIsRequired                = 402,
+    PCHTTPAccessIsForbidden                = 403,
+    PCHTTPResourceWasNotFound              = 404,
+    PCHTTPMethodIsNotAllowed               = 405,
+    PCHTTPContentIsNotAcceptable           = 406,
+    PCHTTPProxyAuthenticationIsRequired    = 407,
+    PCHTTPRequestTimedOut                  = 408,
+    PCHTTPResourceConflict                 = 409,
+    PCHTTPResourceIsGone                   = 410,
+    PCHTTPLengthHeaderIsRequired           = 411,
+    PCHTTPPreconditionFailed               = 412,
+    PCHTTPRequestEntityIsTooLarge          = 413,
+    PCHTTPRequestURIIsTooLong              = 414,
+    PCHTTPMediaTypeIsUnsupported           = 415,
+    PCHTTPRequestedRangeIsUnsatisfiable    = 416,
+    PCHTTPExpectationFailed                = 417,
+    
+    // Server Errors
+    PCHTTPInternalServerError              = 500,
+    PCHTTPResourceNotImpemented            = 501,
+    PCHTTPServerIsABadGateway              = 502,
+    PCHTTPServiceIsUnavailable             = 503,
+    PCHTTPGatewayDidTimeout                = 504,
+    PCHTTPHTTPVersionIsUnsupported         = 505
+    
 } PCHTTPResponseStatus;
 
 /*!
